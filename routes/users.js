d46200accbcbf98c6218970a6a0fc0314721ca74
w2u2u <warunyu.pon@gmail.com>
2017-12-03 02:54:57 +0700

    Register form validation

--- routes/users.js
+++ routes/users.js
@@ -8,4 +8,22 @@ router.get('/login', (req, res) => res.render('users/login'))
 // User Register
 router.get('/register', (req, res) => res.render('users/register'))
 
+router.post('/register', (req, res) => {
+  let errors = []
+
+  if (req.body.password != req.body.confirmPassword)
+    errors.push({ text: "Passwords don't match" })
+  if (req.body.password.length < 4)
+    errors.push({ text: 'Password must be at least 4 characters' })
+  if (errors.length > 0)
+    res.render('users/register', {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword
+    })
+  else res.redirect('/users/login')
+})
+
 module.exports = router
